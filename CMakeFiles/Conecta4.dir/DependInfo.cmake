ecea4d82ff3b6958a740886d10ac48aab740291b
Marco Vasquez <mvsquisix@gmail.com>
2024-05-04 06:19:00 -0700

    Proyecto Conecta 4

---
+++ CMakeFiles/Conecta4.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/acer/CLionProjects/Conecta4/Caracteres.cpp" "C:/Users/acer/CLionProjects/Conecta4/cmake-build-debug-mingw/CMakeFiles/Conecta4.dir/Caracteres.cpp.obj"
+  "C:/Users/acer/CLionProjects/Conecta4/Creditos.cpp" "C:/Users/acer/CLionProjects/Conecta4/cmake-build-debug-mingw/CMakeFiles/Conecta4.dir/Creditos.cpp.obj"
+  "C:/Users/acer/CLionProjects/Conecta4/Gestion.cpp" "C:/Users/acer/CLionProjects/Conecta4/cmake-build-debug-mingw/CMakeFiles/Conecta4.dir/Gestion.cpp.obj"
+  "C:/Users/acer/CLionProjects/Conecta4/Instrucciones.cpp" "C:/Users/acer/CLionProjects/Conecta4/cmake-build-debug-mingw/CMakeFiles/Conecta4.dir/Instrucciones.cpp.obj"
+  "C:/Users/acer/CLionProjects/Conecta4/Jugar.cpp" "C:/Users/acer/CLionProjects/Conecta4/cmake-build-debug-mingw/CMakeFiles/Conecta4.dir/Jugar.cpp.obj"
+  "C:/Users/acer/CLionProjects/Conecta4/Menu.cpp" "C:/Users/acer/CLionProjects/Conecta4/cmake-build-debug-mingw/CMakeFiles/Conecta4.dir/Menu.cpp.obj"
+  "C:/Users/acer/CLionProjects/Conecta4/Prepartida.cpp" "C:/Users/acer/CLionProjects/Conecta4/cmake-build-debug-mingw/CMakeFiles/Conecta4.dir/Prepartida.cpp.obj"
+  "C:/Users/acer/CLionProjects/Conecta4/Utileria.cpp" "C:/Users/acer/CLionProjects/Conecta4/cmake-build-debug-mingw/CMakeFiles/Conecta4.dir/Utileria.cpp.obj"
+  "C:/Users/acer/CLionProjects/Conecta4/main.cpp" "C:/Users/acer/CLionProjects/Conecta4/cmake-build-debug-mingw/CMakeFiles/Conecta4.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Conecta4.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Conecta4.dir/Caracteres.cpp.obj"
+  "CMakeFiles/Conecta4.dir/Creditos.cpp.obj"
+  "CMakeFiles/Conecta4.dir/Gestion.cpp.obj"
+  "CMakeFiles/Conecta4.dir/Instrucciones.cpp.obj"
+  "CMakeFiles/Conecta4.dir/Jugar.cpp.obj"
+  "CMakeFiles/Conecta4.dir/Menu.cpp.obj"
+  "CMakeFiles/Conecta4.dir/Prepartida.cpp.obj"
+  "CMakeFiles/Conecta4.dir/Utileria.cpp.obj"
+  "CMakeFiles/Conecta4.dir/main.cpp.obj"
+  "Conecta4.exe"
+  "Conecta4.exe.manifest"
+  "Conecta4.pdb"
+  "libConecta4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Conecta4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
